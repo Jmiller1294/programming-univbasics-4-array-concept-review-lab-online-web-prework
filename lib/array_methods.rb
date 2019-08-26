3e26d5f6f7b5c626cc353a8b78a5de38998bb867
Justin Miller <jayman1294@gmail.com>
2019-08-26 03:48:50 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -13,10 +13,9 @@ def find_max_value(array)
   x = 0
   array.length.times do |count|
     if array[count] > x
-    return x
+    x = array[count]
+    end
   end
-  end
-    
 end
 
 def find_min_value(array)
